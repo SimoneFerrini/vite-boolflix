cd1ac0c218c3269ea355df8215ec5241eef3a08d
SimoneFerrini <ferrini.simone4@gmail.com>
2023-03-30 15:40:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+// API key: 9c8e071c9d2477e79c9ef99320d11610
+
+<script>
+import {store} from "./store";
+import axios from "axios";
+
+export default{
+  data(){
+    return{
+
+        store,
+    }
+  },
+
+  created(){
+    axios.get().then((res) => {
+
+    });
+  },
+
+
+
+  
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
